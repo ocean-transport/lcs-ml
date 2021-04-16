b6a094a0d1c177f303594d33bf7df39aba82171b
“hscannell” <scannell@ldeo.columbia.edu>
2021-04-15 21:41:27 -0400

    running job-array!

---
+++ ensemble_generator.py
@@ -0,0 +1,51 @@
+import numpy as np
+import xarray as xr
+import pyqg
+from numpy.random import Generator, MT19937, SeedSequence
+import os
+
+# parameter for randomness
+n = os.getenv('SLURM_ARRAY_TASK_ID')
+
+# initial conditions from equilibrium run
+ds_initial = xr.open_dataset('/burg/abernathey/users/hillary/QG_equilibrium_proto.nc')
+initial_PV = ds_initial.q.values
+
+# model configuration
+year = 24*60*60*360.
+day = 24*60*60.
+tmax = year
+twrite = day
+tavestart = day
+    
+def ensemble_generator(initial_PV, n):
+    
+    # configure model
+    m = pyqg.QGModel(tmax=tmax, twrite=twrite, tavestart=tavestart)
+
+    # create an empty array filled with zeros 
+    noise = np.zeros_like(ds_initial.q.values)
+
+    # index to the middle of noise and add random perturbation to both levels
+    rg = Generator(MT19937(SeedSequence(n)))
+    noise[:,np.floor(len(ds_initial.x)/2).astype('int'), 
+          np.floor(len(ds_initial.y)/2).astype('int')] = sig*rg.random((m.q.shape[0]))
+
+    # set PV anomaly with randomness
+    m.set_q(ds_initial.q.values + noise)
+    
+    # run with snapshots, save model increments as xarray DataSet
+    datasets = []
+    for snapshot in m.run_with_snapshots(tsnapstart=m.t, tsnapint=m.dt):
+        model = m.to_dataset()
+        model = model.assign(q=(('time','lev','y','x'), m.q.copy()[np.newaxis,...]))
+        datasets.append(model)
+
+    ds = xr.concat(datasets, dim='time')
+  
+    # Save model to netCDF
+    fn = '/burg/abernathey/users/hillary/QG_proto_EM_'+ str('%03d'%n) +'.nc'
+    ds.to_netcdf(fn, engine='h5netcdf', invalid_netcdf=True)
+    
+# generate ensemble
+ensemble_generator(initial_PV, n)
---
+++ ensemble_generator.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Generates ensembles of QG model run from equilibrated state
+#
+#SBATCH --account=abernathey     # group account name
+#SBATCH --job-name=ensembles     # job name
+#SBATCH -c 1                     # number of cpu cores to use (up to 32 cores per server)
+#SBATCH --time=0-02:00           # time the job will take to run in D-HH:MM
+#SBATCH --mem-per-cpu=5G         # The memory the job will use per cpu core
+ 
+echo "$SLURM_ARRAY_TASK_ID"
+
+module load anaconda
+source ~/.bashrc
+conda activate lcs-dev
+
+python ensemble_generator.py
