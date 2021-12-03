9da93fd9b39553cd46ac2fb85f5eaa03ee388dc5
“hscannell” <scannell@ldeo.columbia.edu>
2021-12-03 16:50:10 -0500

    set pickup file new

--- ensemble_particle_generator/ensemble_generator.sh
+++ ensemble_particle_generator/ensemble_generator.sh
@@ -4,8 +4,8 @@
 #
 #SBATCH --account=abernathey     # group account name
 #SBATCH --job-name=ensembles     # job name
-#SBATCH -c 5                     # number of cpu cores to use (up to 32 cores per server)
-#SBATCH --time=0-02:00           # time the job will take to run in D-HH:MM
+#SBATCH -c 1                     # number of cpu cores to use (up to 32 cores per server)
+#SBATCH --time=0-06:00           # time the job will take to run in D-HH:MM
 #SBATCH --mem-per-cpu=20G         # The memory the job will use per cpu core
  
 echo "$SLURM_ARRAY_TASK_ID"
